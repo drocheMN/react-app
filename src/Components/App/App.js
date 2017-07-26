0baafabe6f0531d28d11b73f2598256eb77fa42b
Derek Roche <droche.dev@gmail.com>
2017-07-26 02:06:21 -0500

    Initial folder structure, add material ui

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../../logo.svg';
 import './App.css';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './index.css';
-import App from './App';
+import App from './Components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+const Root = () => (
+    <MuiThemeProvider>
+        <App />
+    </MuiThemeProvider>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
+
